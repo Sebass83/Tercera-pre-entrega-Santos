3bd4fd7626184103352324f768b5ae8c9e220eb5
Sebass83 <ssantoss@gmail.com>
2023-09-19 20:57:59 -0300

    The models are added to the database and its migration is carried out.

--- CoderBlogApp/models.py
+++ CoderBlogApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class blogSummary(models.Model):
+    titles = models.CharField(max_length=200, unique=True)
+    description = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    entryDate = models.DateField(auto_now_add=True)
+    min_img_url= models.URLField()
+
+
+class blog(models.Model):
+    post = models.CharField(max_length=200)
+    hero_img_url = models.URLField
+    quote = models.CharField(max_length=30)
+    acent_img_url= models.URLField()
+
+class entryBlog(models.Model):
+    author = models.CharField(max_length=200)
+    entryDate = models.DateField(auto_now_add=True)
+    slug = models.CharField(max_length=200, unique=True)
+    
