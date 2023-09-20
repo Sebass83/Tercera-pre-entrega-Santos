426464e51df16bb88c0c594814747e388ea56757
Sebass83 <ssantoss@gmail.com>
2023-09-19 21:05:40 -0300

    The name of the models is corrected, they are migrated and registered.

--- CoderBlogApp/admin.py
+++ CoderBlogApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.register(BlogSummary)
+admin.register(Blog)
+admin.register(EntryBlogData)
--- CoderBlogApp/models.py
+++ CoderBlogApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class blogSummary(models.Model):
+class BlogSummary(models.Model):
     titles = models.CharField(max_length=200, unique=True)
     description = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
@@ -10,13 +10,13 @@ class blogSummary(models.Model):
     min_img_url= models.URLField()
 
 
-class blog(models.Model):
+class Blog(models.Model):
     post = models.CharField(max_length=200)
     hero_img_url = models.URLField
     quote = models.CharField(max_length=30)
     acent_img_url= models.URLField()
 
-class entryBlog(models.Model):
+class EntryBlogData(models.Model):
     author = models.CharField(max_length=200)
     entryDate = models.DateField(auto_now_add=True)
     slug = models.CharField(max_length=200, unique=True)
